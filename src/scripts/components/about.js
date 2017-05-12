ec22e685451c4a2c31001343607d648c7c208e94
Andrew Mason <andrewmason1@gmail.com>
2017-05-12 12:42:41 -0400

    skills responsiveness

--- src/scripts/components/about.js
+++ src/scripts/components/about.js
@@ -3,19 +3,27 @@ import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Waypoint from 'react-waypoint';
 
 export const AboutComp = React.createClass({
+
+  // handleWaypointEnter: function(){
+  //
+  //   <Waypoint
+  //     onEnter={this.handleWaypointEnter}
+  //     onLeave={this.handleWaypointLeave}
+  //     threshold={50}
+  //   />
+  // },
+
   render: function(){
     return (
       <div  id="about" className="about-container">
-        {/* <Waypoint> */}
-          <CSSTransitionGroup
-            transitionName="abouttitle"
-            transitionAppear={true}
-            transitionAppearTimeout={2000}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <h2>ABOUT</h2>
-          </CSSTransitionGroup>
-      {/* </Waypoint> */}
+        <CSSTransitionGroup
+          transitionName="abouttitle"
+          transitionAppear={true}
+          transitionAppearTimeout={2000}
+          transitionEnter={false}
+          transitionLeave={false}>
+          <h2>ABOUT</h2>
+        </CSSTransitionGroup>  
         <div className="headshot">
           <img src="images/IMG_1821.png"/>
         </div>
--- src/scripts/components/skills.js
+++ src/scripts/components/skills.js
@@ -3,33 +3,44 @@ import React from 'react';
 export const SkillsComp = React.createClass({
   render: function(){
     return (
-      <div id="skills" className="skills-container">
+      <div id="skills" className="container skills-container">
         <h1>SKILLS</h1>
-        <ul>
-          <h2>Web Development</h2>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>Sass</li>
-          <li>Bootstrap</li>
-        </ul>
-        <ul>
-          <h2>Javascript</h2>
-          <li>Vanilla JS</li>
-          <li>React</li>
-          <li>Backbone.js</li>
-        </ul>
-        <ul>
-          <h2>Dev Tools</h2>
-          <li>Git</li>
-          <li>GitHub</li>
-          <li>npm</li>
-          <li>Command Line</li>
-        </ul>
-        <div className="res-link">
-          <h2>View full resume here.</h2>
-          <a href="https://drive.google.com/open?id=0B1OsERFXiXayTUdYSnM4cmpjSFE" target="_blank"><i className="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-lg-4">
+              <ul className="thumbnail">
+                <h2>Web Development</h2>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+              </ul>
+            </div>
+            <div className="col-sm-12 col-lg-4">
+              <ul className="thumbnail">
+                <h2>Javascript</h2>
+                <li>Vanilla JS</li>
+                <li>React</li>
+                <li>Backbone.js</li>
+              </ul>
+            </div>
+            <div className="col-sm-12 col-lg-4">
+              <ul className="thumbnail">
+                <h2>Dev Tools</h2>
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>npm</li>
+                <li>Command Line</li>
+              </ul>
+            </div>
+          </div>
+          </div>
+          <div className="res-link">
+            <h2>View full resume here.</h2>
+            <a href="https://drive.google.com/open?id=0B1OsERFXiXayTUdYSnM4cmpjSFE" target="_blank"><i className="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+          </div>
         </div>
-      </div>
+
     )
   }
 })
